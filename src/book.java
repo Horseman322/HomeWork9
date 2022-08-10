fea3d7648ed0320c6b709127c65a610dd47c2241
Kosarev <kosarevn97@gmail.com>
2022-08-10 23:08:51 +0700

    Сделал дз

--- src/book.java
+++ src/book.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class book {
+public class book {
+
+    private final String name;
+    private int year;
+    private final author author;
+
+    public book(String name, int year, author author) {
+        this.name = name;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public author getAuthor() {
+        return author;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
